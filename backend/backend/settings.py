e0506b2d21179847f6fc41272807f27ad54ee356
Артём Серов (д) <artyomserov@yandex.ru>
2023-10-06 03:02:45 +0500

    Change settigs.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,15 @@
 import os
 from pathlib import Path
+from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 
 # Application definition
