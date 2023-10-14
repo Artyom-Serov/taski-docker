475f5e493ad7fee32781bc6b5328ff1dfedeaf7b
Артём Серов (д) <artyomserov@yandex.ru>
2023-10-14 18:41:44 +0500

    War with errors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,9 +10,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY: str = os.getenv('SECRET_KEY', 'django-insecure')
 # DEBUG: bool = os.getenv('DEBUG') == 'True'
 # ALLOWED_HOSTS: list[str] = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['130.193.34.161', '127.0.0.1', 'localhost', 'purposes.sytes.net']
+
+# SECRET_KEY = os.getenv('SECRET_KEY')
+# DEBUG = os.getenv('DEBUG') == 'True'
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
